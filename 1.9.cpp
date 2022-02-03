2a4efc4e0418c1e71b6075cb16971bc894458217
darynaShalaenko <98517299+darynaShalaenko@users.noreply.github.com>
2022-02-03 19:07:19 +0200

    Update 1.9.cpp

--- 1.9.cpp
+++ 1.9.cpp
@@ -12,7 +12,8 @@ int main()
 	scanf_s("%lf", &y);
 	Harmonic = 2 / ((1 / x) + (1 / y));
 	Arithmetic = (x + y) / 2;
-	printf("Arithmetic mean:   %lf, %ef\nHarmonic mean:   %lf, %ef\n", Arithmetic, Arithmetic, Harmonic, Arithmetic);
+	printf("Arithmetic mean:   %lf, %ef\nHarmonic mean:   %lf, %ef\n", Arithmetic, Arithmetic, Harmonic, Harmonic
+	      );
 
 
 
