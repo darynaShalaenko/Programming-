38c2a56ce3d3c1ffac338f36042c3048dc0ffe2e
darynaShalaenko <98517299+darynaShalaenko@users.noreply.github.com>
2022-02-04 00:14:43 +0200

    1.24 (Shalaenko Daryna)

---
+++ 1.24.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1, s2, s_res;
+	int n, m;
+	cin >> n >> m;
+	s1.resize(n, ' ');
+	s2.resize(2 * n, ' ');
+	cout << s1 << m << endl;
+	cout << s2 << pow(m, 2) << endl;
+	
+
+
+}
+
+
+
+
+
+
