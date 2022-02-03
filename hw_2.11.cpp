74f6de7bc950c3072e8a849ad8008af297e2cbe8
darynaShalaenko <98517299+darynaShalaenko@users.noreply.github.com>
2022-02-03 19:26:17 +0200

    Home_work_2

---
+++ hw_2.11.cpp
@@ -0,0 +1,18 @@
+﻿
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double r, h, square, volume;
+    printf("The radius = ");
+    scanf_s("%lf", &r);
+    printf("The hight of the base = ");
+    scanf_s("%lf", &h);
+    square = M_PI*pow(r,2);
+    volume = square * h;
+    printf("Volume of cylinder = %lf\n", volume);
+
+}
+
