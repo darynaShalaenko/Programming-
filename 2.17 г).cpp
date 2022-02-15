96a6493dfc26c21de1f60f0f6c55f57018be3008
darynaShalaenko <98517299+darynaShalaenko@users.noreply.github.com>
2022-02-15 22:44:46 +0200

    2.17 г); (Shalaenko Daryna)

---
+++ 2.17 г).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+double foo(double x) {
+    
+    return pow(tan(x),-1);
+}
+
+double derivative(double x) {
+    return 1 / (1 + pow(x, 2));
+}
+
+
+int main()
+{
+    double x;
+    printf("Enter x: ");
+    cin >> x;
+    cout << "Function value = " << foo(x) << "\n" << "Derivative function = " << derivative(x);
+    return 0;
+}
