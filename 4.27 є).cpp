e6f5ccf2f5a89b3ac24eddd77acad95766a98f81
darynaShalaenko <98517299+darynaShalaenko@users.noreply.github.com>
2022-05-06 21:25:58 +0300

    4.27 є)

---
+++ 4.27 є).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+double foo(double k)
+{
+    double p = 2 * sqrt(3) * ((pow(-1, k)) / (pow(3, k) * (2 * k + 1)));
+    return p;
+
+
+}
+
+
+int main()
+{
+    double k;
+    cin >> k;
+    cout << foo(k);
+
+
+    return 0;
+}
