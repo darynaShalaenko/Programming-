e8d70a2c023a4c08aaefde68d02ac22cdab041ca
darynaShalaenko <98517299+darynaShalaenko@users.noreply.github.com>
2022-05-31 00:57:56 +0300

    12.(1-5) (Shalaenko Daryna)

---
+++ t12.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+int main1() {
+	double x, y;
+	std::cin >> x;
+	std::cin >> y;
+	std::cout << std::fixed << "x^y=" << pow(x, y) << std::endl;
+	std::cout << std::scientific << "x^y=" << pow(x, y);
+	return 0;
+
+}
