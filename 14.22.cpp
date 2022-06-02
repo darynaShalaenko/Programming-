eba488697b5eb61f6ee9865e0cb764773c66e0d1
darynaShalaenko <98517299+darynaShalaenko@users.noreply.github.com>
2022-06-02 11:26:50 +0300

    14.22 (Shalaenko Daryna)

---
+++ 14.22.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+    string line;
+    ifstream infile("F.txt");
+    ofstream outfile1("H.txt");
+    ofstream outfile2("G.txt");
+    string ans = "";
+    string word = "";
+    string text;
+    char sym;
+    if (infile.is_open()) {
+        while (getline(infile, text, '\n')) {
+            if (text.size() % 2 == 0) {
+                outfile2 << text << "\n";
+            }
+            else {
+                outfile1 << text << "\n";
+            }
+        }
+        infile.close();
+    }
+    outfile1.close();
+    outfile2.close();
+    return 0;
+}
