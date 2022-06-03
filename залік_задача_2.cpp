2528f40866529066160b361582ccb12eb284fbc8
darynaShalaenko <98517299+darynaShalaenko@users.noreply.github.com>
2022-06-03 16:47:53 +0300

    Білет_№14 Шалаєнко Дарина

---
+++ залік_задача_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    string file_string, max;
+
+    ifstream file;
+    file.open("Текст.txt");
+    if (file.is_open()) {
+        while (file.good()) {
+            getline(file, file_string);
+            if (max.length() < file_string.length()) {
+                max = file_string;
+            }
+        }
+        file.close();
+        cout << "Максимальний рядок: " << endl << " " << max << endl;
+    }
+    else {
+        cerr << "Неможливо відкрити файл!" << endl;
+    }
+
+   
+    return 0;
+}
